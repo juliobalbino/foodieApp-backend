821f2bedf653da2ef41ed961be027bfb74c94c20
Julio Balbino <julio4110@hotmail.com>
2020-07-08 17:08:20 -0300

    Projeto criado

---
+++ src/main/java/com/foodie/foodieApp/FoodieAppApplication.java
@@ -0,0 +1,13 @@
+package com.foodie.foodieApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodieAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodieAppApplication.class, args);
+	}
+
+}
