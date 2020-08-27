9f24a92023a07eab23a830fdbaa147522f1914cf
Julio Balbino <julio4110@hotmail.com>
2020-08-27 16:14:12 -0300

    Configurando e salvando arquivo no AmazonS3

--- src/main/java/com/foodie/foodieApp/FoodieAppApplication.java
+++ src/main/java/com/foodie/foodieApp/FoodieAppApplication.java
@@ -1,13 +1,26 @@
 package com.foodie.foodieApp;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class FoodieAppApplication {
+import com.foodie.foodieApp.services.S3Service;
 
+@SpringBootApplication
+public class FoodieAppApplication implements CommandLineRunner{
+
+	@Autowired
+	private S3Service s3Service;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(FoodieAppApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		s3Service.uploadFile("D:\\Dev\\Projetos\\ws-sts\\fotos\\cr1.jpg");
+		
 	}
 
 }
