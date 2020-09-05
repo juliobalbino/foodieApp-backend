79bd2f4eef7c5628b5abfad2438886191e6e5c1f
Julio Balbino <julio4110@hotmail.com>
2020-09-04 21:18:21 -0300

    Fix: Producao

---
+++ src/main/java/com/foodie/foodieApp/config/ProdConfig.java
@@ -0,0 +1,38 @@
+package com.foodie.foodieApp.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.foodie.foodieApp.services.DBService;
+import com.foodie.foodieApp.services.EmailService;
+import com.foodie.foodieApp.services.SmtpEmailService;
+
+@Configuration
+@Profile("prod")
+public class ProdConfig {		
+		
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean instantiateDatabase() {
+		
+		if (!"create".equals(strategy)) {
+			return false;
+		}
+		
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
+	}
+}
